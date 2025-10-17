6f7a6142252b5c677ba7d7876cc131851034899d
inclufour <omiprakhar@gmail.com>
2025-10-17 09:05:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -35,4 +35,10 @@ val_ds = train_gen.flow_from_directory(
 # Building the CNN Model
 model = models.Sequential([
     layers.Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)),
-    layers.MaxPooling
+    layers.MaxPooling2D(2,2),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
